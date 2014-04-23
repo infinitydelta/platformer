9aadbefb28a37a02543c431111c2e093f89a7ce6
infinitydelta <kylesan123@gmail.com>
2014-04-23 00:49:03 -0700

    shooting, moving

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+	Vector3 pos;
+	GameObject[] playerz;
+	// Use this for initialization
+	void Start () {
+		playerz = GameObject.FindGameObjectsWithTag ("Player");
+		//pos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		pos = Vector3.zero;
+		for (int i = 0; i < playerz.Length; i ++) {
+
+			pos += playerz[i].transform.position;
+		}
+		pos /= playerz.Length;
+		transform.position = (new Vector3(pos.x, pos.y, -10) );
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	bool moving, movingX, movingY, jumping;
+
+	public int regularSpeed = 5;
+	public int speed;
+	public float stoppingFriction = .85f;
+	public GameObject projectile;
+	public GameObject gunTip;
+	// Use this for initialization
+	void Start () {
+		speed = regularSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moving = (movingX || movingY);
+
+		movingX = false;
+
+		controls ();
+		if (rigidbody2D.velocity.y == 0) {
+			movingY = false;
+		} else {
+			movingY = true;
+		}
+		/*
+		if (movingY) {
+			speed = regularSpeed /2;
+		}
+		else {
+			speed = regularSpeed;
+		} */
+
+		if (!movingX) { //stop
+			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x * stoppingFriction, rigidbody2D.velocity.y);
+		}
+		//rigidbody2D.AddForce (new Vector2 (0, -500));
+		if (rigidbody2D.velocity.y <= -10) {
+			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, -10);
+		}
+	}
+
+	void controls() {
+		if (Input.GetKey (KeyCode.A)) {
+			movingX = true;
+			transform.localScale = new Vector3(-1,1,1);
+			rigidbody2D.velocity = new Vector2(-speed, rigidbody2D.velocity.y);	
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			movingX = true;
+			transform.localScale = new Vector3(1,1,1);
+			rigidbody2D.velocity = new Vector2(speed, rigidbody2D.velocity.y);	
+		}
+
+		if (Input.GetKey(KeyCode.Space) && !movingY) {
+			//rigidbody2D.AddForce(new Vector2(0, 400));
+			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 16);
+			movingY = true;
+			jumping = true;
+		}
+		if (Input.GetKeyDown (KeyCode.J)) {
+			Quaternion rot = Quaternion.Euler(0,0,0);
+			if (transform.localScale.x < 0 ) {
+				rot = Quaternion.Euler(0,0,180);
+				//print (rot.eulerAngles.z);
+			}
+		
+			Instantiate (projectile, gunTip.transform.position, rot); 
+		}
+	}
+}
